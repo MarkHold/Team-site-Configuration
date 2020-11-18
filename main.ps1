f905410df6f0edf58e05761ecbab8e631c32f920
Markus Lykonhold <lykonhold@gmail.com>
2020-11-18 22:28:22 +0100

    switching over to PNP

--- main.ps1
+++ main.ps1
@@ -1,2 +1,8 @@
-﻿#login
-$cred = Get-Credential
+﻿Connect-PnPOnline "https://[tenant].sharepoint.com/"
+
+# Create the new "modern" team site
+$teamSiteUrl = New-PnPSite -Type TeamSite -Title "MarkusSite" -Alias "mymodernteamsite" -Description "description" -IsPublic -Classification "classification" 
+
+# Connect to the modern site using PnP PowerShell SP cmdlets
+# Since we are connecting now to SP side, credentials will be asked
+Connect-PnPOnline $teamSiteUrl
